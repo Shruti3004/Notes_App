4a8076749db582968183f8d7cf79f77d7487dcee
Shruti3004 <mail2shruti.ag@gmail.com>
2020-11-20 04:00:39 +0530

    added email validator

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+const validator = require('validator');
 const getNotes = require("./notes");
 
 const msg = getNotes();
 console.log(msg);
+
+console.log(validator.isEmail('mail2shruti.ag@gmail.com'))
+console.log(validator.isURL('mail2shruti.ag@gmail.com'))
