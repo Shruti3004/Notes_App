69807064c713cfde8ccbca48b46f0d6e73e38302
Shruti3004 <mail2shruti.ag@gmail.com>
2020-11-21 03:02:23 +0530

    Argument parsing with Yargs part 1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 // const getNotes = require("./notes");
 
 // const msg = getNotes();
@@ -14,10 +15,55 @@ const chalk = require('chalk');
 // it always provide two arrays in the aray 1st from where this node execution is taking place and second the loaction of directory
 // console.log(process.argv)
 
-const command = process.argv[2];
+// const command = process.argv[2];
 
-if(command === 'add'){
-    console.log('Adding Note!!!')
-}else{
-    console.log(command)
-}
+// if(command === 'add'){
+//     console.log('Adding Note!!!')
+// }else{
+//     console.log(command)
+// }
+
+// Customize version
+yargs.version('1.1.0')
+
+// Create Add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function (){
+        console.log('Adding a new note')
+    }
+})
+
+// Create Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function (){
+        console.log('Removing a new note')
+    }
+})
+
+// Create Read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a new note',
+    handler: function (){
+        console.log('Reading a new note')
+    }
+})
+
+// Create List command
+yargs.command({
+    command: 'list',
+    describe: 'List a new note',
+    handler: function (){
+        console.log('Listing a new note')
+    }
+})
+
+// Add, Remove, Read, List
+
+// Difference between yargs and process
+console.log(yargs.argv)
+// console.log(process.argv)
