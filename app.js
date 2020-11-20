1fed03f0696ee0f757227b169bed6aa2313d4395
Shruti3004 <mail2shruti.ag@gmail.com>
2020-11-21 02:30:50 +0530

    getting input from user

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const validator = require('validator');
 const chalk = require('chalk');
-const getNotes = require("./notes");
+// const getNotes = require("./notes");
 
-const msg = getNotes();
-console.log(msg);
+// const msg = getNotes();
+// console.log(msg);
 
-console.log(validator.isEmail('mail2shruti.ag@gmail.com'))
-console.log(validator.isURL('mail2shruti.ag@gmail.com'))
-console.log(chalk.redBright.bold.inverse('Hello World'));
+// console.log(validator.isEmail('mail2shruti.ag@gmail.com'))
+// console.log(validator.isURL('mail2shruti.ag@gmail.com'))
+// console.log(chalk.redBright.bold.inverse('Hello World'));
+
+// Taking the inputs from command prompt
+// just like we have window in javascript same as process in node.js
+// it always provide two arrays in the aray 1st from where this node execution is taking place and second the loaction of directory
+// console.log(process.argv)
+
+const command = process.argv[2];
+
+if(command === 'add'){
+    console.log('Adding Note!!!')
+}else{
+    console.log(command)
+}
